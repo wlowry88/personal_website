69bdfd4369819cc555bf6ae8ee4500aa84a82eac
Will Lowry <william.c.lowry@gmail.com>
2014-08-16 19:38:44 -0400

    fixes some links

--- app/assets/javascripts/designr.js
+++ app/assets/javascripts/designr.js
@@ -115,7 +115,7 @@ $(document).ready(function() {
 	Project Preview
 	==============================================*/
 	$('.project-item').click(function(e){
-		e.preventDefault();
+
 
 		var elem = $(this),
 			title = elem.find('.project-title').text(),
